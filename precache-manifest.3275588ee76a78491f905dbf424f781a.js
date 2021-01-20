0a79416fbdc813cc4985b33b802a645262840669
PRODI <prodi.ofic@gmail.com>
2021-01-20 11:09:06 +0300

    Updates

---
+++ precache-manifest.3275588ee76a78491f905dbf424f781a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f97674de5a5a8731280b6425e3a38fa",
+    "url": "/accord/index.html"
+  },
+  {
+    "revision": "4152b427d61cc3c870fc",
+    "url": "/accord/static/css/3.aa94fa51.chunk.css"
+  },
+  {
+    "revision": "bb784ca9044a453a6eea",
+    "url": "/accord/static/css/4.409cfab4.chunk.css"
+  },
+  {
+    "revision": "8769ddca02eea34a1a08",
+    "url": "/accord/static/css/5.1a618e48.chunk.css"
+  },
+  {
+    "revision": "75bddf70acb4d3c58cb6",
+    "url": "/accord/static/css/6.fd1f9338.chunk.css"
+  },
+  {
+    "revision": "daeeaed8936ccc68e89a",
+    "url": "/accord/static/css/7.34e8a3e8.chunk.css"
+  },
+  {
+    "revision": "6ee00d994a752fc7306f",
+    "url": "/accord/static/css/main.8ffdc78e.chunk.css"
+  },
+  {
+    "revision": "fea6e3b51e424e8b8b75",
+    "url": "/accord/static/js/2.2aa26381.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/accord/static/js/2.2aa26381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4152b427d61cc3c870fc",
+    "url": "/accord/static/js/3.a0033833.chunk.js"
+  },
+  {
+    "revision": "bb784ca9044a453a6eea",
+    "url": "/accord/static/js/4.689149bc.chunk.js"
+  },
+  {
+    "revision": "8769ddca02eea34a1a08",
+    "url": "/accord/static/js/5.76579f80.chunk.js"
+  },
+  {
+    "revision": "75bddf70acb4d3c58cb6",
+    "url": "/accord/static/js/6.80848e20.chunk.js"
+  },
+  {
+    "revision": "daeeaed8936ccc68e89a",
+    "url": "/accord/static/js/7.2220fad1.chunk.js"
+  },
+  {
+    "revision": "6ee00d994a752fc7306f",
+    "url": "/accord/static/js/main.6aa190fc.chunk.js"
+  },
+  {
+    "revision": "c948546a2068946fb968",
+    "url": "/accord/static/js/runtime-main.1af4b9ad.js"
+  },
+  {
+    "revision": "5e946c20186e33b608fbb3388fff6ff5",
+    "url": "/accord/static/media/users-avatar.5e946c20.svg"
+  }
+]);
